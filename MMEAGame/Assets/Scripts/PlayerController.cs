9bba824d5dda8017db29c16d41ec6f14838d2469
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-08 20:55:04 +0300

    douple jump

--- MMEAGame/Assets/Scripts/PlayerController.cs
+++ MMEAGame/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,15 @@ public class PlayerController : MonoBehaviour
     // Serialized Fields
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private float _doupleJumpForce;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private LayerMask _groundLayerMask;
 
     // Basic Variables
     public Rigidbody2D playerRigidbody;
     private bool isOnGround;
+    private bool canDoubleJump; 
+    
     
 
 
@@ -35,12 +38,24 @@ public class PlayerController : MonoBehaviour
         playerRigidbody.velocity = new Vector2(_moveSpeed * Input.GetAxis("Horizontal"), playerRigidbody.velocity.y);
         
         // MAX: Jump
-        isOnGround = Physics2D.OverlapCircle(_groundCheck.position, .2f, _groundLayerMask); // MAX: Check if there is a collision with any other objects (Circle Object)
+        isOnGround = Physics2D.OverlapCircle(_groundCheck.position, .2f, _groundLayerMask); // MAX: Check if there is a collision with any other objects (Circle Objct on Ground)
+        if (isOnGround)
+        {
+            canDoubleJump = true;
+        }
         if (Input.GetButtonDown("Jump"))
         {
             if (isOnGround)
             {
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, _jumpForce);    
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, _jumpForce);
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, _doupleJumpForce);
+                    canDoubleJump = false;
+                }
             }
         }
     }

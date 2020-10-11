7fcee7aef8f08a172a36387279c2c338ad5bb080
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-11 18:09:19 +0300

    camera movement implemented

--- MMEAGame/Assets/Scripts/CameraController.cs
+++ MMEAGame/Assets/Scripts/CameraController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public Transform target;
+    public Transform backgroundFar, backgroundMiddle;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
     }
 }

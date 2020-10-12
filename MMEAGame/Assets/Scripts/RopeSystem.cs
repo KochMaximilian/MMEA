f9faf77bfaad74ea6f0810d59156348bb8d177ef
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-12 15:46:23 +0300

    grapling calculation

--- MMEAGame/Assets/Scripts/RopeSystem.cs
+++ MMEAGame/Assets/Scripts/RopeSystem.cs
@@ -18,8 +18,8 @@ public class RopeSystem : MonoBehaviour
     public GameObject ropeHingeAnchor;
     public DistanceJoint2D ropeJoint;
     public Transform crosshair;
-    public SpriteRenderer crosshairSprite;
-   // public PlayerMovement PlayerMovement;
+    public SpriteRenderer crosshairSprite; 
+    public PlayerController PlayerController;
     private bool ropeAttached;
     private Vector2 playerPosition;
     private Rigidbody2D ropeHingeAnchorRb;
@@ -39,15 +39,17 @@ public class RopeSystem : MonoBehaviour
         // MAX_ Get position of the mouse with ScreenToWorldPoint method
         var worldMousePosition =
             Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
-        var facingDirection = worldMousePosition - transform.position;
-        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        var facingDirection = worldMousePosition - transform.position; // MAX: calculate facing direction by subtracting player position from the mouse position in the world
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x); // MAX: represents the Angel of the mouse cursor
         if (aimAngle < 0f)
         {
+            // MAX: Keep the value positive
             aimAngle = Mathf.PI * 2 + aimAngle;
         }
 
+        //MAX: convert the radian angle to an angle in degrees
         var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
-        playerPosition = transform.position;
+        playerPosition = transform.position;  // MAX: Track player position 
 
         if (!ropeAttached)
         {
@@ -58,6 +60,8 @@ public class RopeSystem : MonoBehaviour
             
         }
     }
+    
+    
     
 
 

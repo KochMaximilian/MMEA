c95d92f386334030820ddc06c29b256af3b3b30c
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-31 00:16:24 +0200

    enemy setupW

---
+++ MMEAGame/Assets/Scripts/EnemieController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Transform _leftPoint, _rightPoint;
+    private bool movingRight;
+    private Rigidbody2D enemyRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        _leftPoint.parent = null;
+        _rightPoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

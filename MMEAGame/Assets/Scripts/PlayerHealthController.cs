151a9fd6dd289c43c46a0f07cdfe6cffb245c953
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-30 16:53:46 +0200

    first step health systemW

---
+++ MMEAGame/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+
+    [SerializeField] private int _currentHealth, _maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        _currentHealth--;
+
+        if (_currentHealth <= 0)
+        {
+            gameObject.SetActive(false); // Max: Player disappears 
+        }
+    }
+}

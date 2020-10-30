3b67998893f47eaa6f31dc4df1e3cfebd13dc59c
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-30 18:34:38 +0200

    added more health

--- MMEAGame/Assets/Scripts/UIController.cs
+++ MMEAGame/Assets/Scripts/UIController.cs
@@ -9,7 +9,7 @@ public class UIController : MonoBehaviour
     public static UIController instance;
     
     [SerializeField] private Image _health1, _health2, _health3; // Max: UI uses Image Components instead of sprites
-    [SerializeField] private Sprite _healthFull, _healthEmpty;
+    [SerializeField] private Sprite _healthFull, _healthEmpty, _healthHalf;
     private void Awake()
     {
         instance = this;
@@ -31,20 +31,38 @@ public class UIController : MonoBehaviour
     {
         switch (PlayerHealthController.instance.currentHealth)
         {
-            case 3:
+            case 6:
                 _health1.sprite = _healthFull;
                 _health2.sprite = _healthFull;
                 _health3.sprite = _healthFull;
                 break;
             
-            case 2:
+            case 5:
+                _health1.sprite = _healthFull;
+                _health2.sprite = _healthFull;
+                _health3.sprite = _healthHalf;
+                break;
+            
+            case 4:
                 _health1.sprite = _healthFull;
                 _health2.sprite = _healthFull;
                 _health3.sprite = _healthEmpty;
                 break;
             
-            case 1:
+            case 3:
                 _health1.sprite = _healthFull;
+                _health2.sprite = _healthHalf;
+                _health3.sprite = _healthEmpty;
+                break;
+            
+            case 2:
+                _health1.sprite = _healthFull;
+                _health2.sprite = _healthEmpty;
+                _health3.sprite = _healthEmpty;
+                break;
+            
+            case 1:
+                _health1.sprite = _healthHalf;
                 _health2.sprite = _healthEmpty;
                 _health3.sprite = _healthEmpty;
                 break;

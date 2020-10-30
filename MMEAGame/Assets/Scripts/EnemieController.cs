e25df2bb1a589358b11fdbc37dfbf3f857644be9
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-10-31 01:18:39 +0200

    finished frog animation

--- MMEAGame/Assets/Scripts/EnemieController.cs
+++ MMEAGame/Assets/Scripts/EnemieController.cs
@@ -6,10 +6,13 @@ public class EnemieController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Transform _leftPoint, _rightPoint;
+    [SerializeField] private float _moveTime, _waitTime;
+    private float moveCount, waitCount;
     private bool movingRight;
     private Rigidbody2D enemyRigidbody;
     public SpriteRenderer enemySpriteRenderer;
-
+    private Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,28 +20,51 @@ public class EnemieController : MonoBehaviour
         _leftPoint.parent = null;
         _rightPoint.parent = null;
         movingRight = true;
+        moveCount = _moveTime;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        if (movingRight)
+        if (moveCount > 0)
         {
-            enemyRigidbody.velocity = new Vector2(_moveSpeed, enemyRigidbody.velocity.y);
-            enemySpriteRenderer.flipX = true;
-            if (transform.position.x > _rightPoint.position.x)
+
+            moveCount -= Time.deltaTime;
+            
+            if (movingRight)
             {
-                movingRight = false;
+                enemyRigidbody.velocity = new Vector2(_moveSpeed, enemyRigidbody.velocity.y);
+                enemySpriteRenderer.flipX = true;
+                if (transform.position.x > _rightPoint.position.x)
+                {
+                    movingRight = false;
+                }
             }
-        }
-        else
-        { 
-            enemyRigidbody.velocity = new Vector2(-_moveSpeed, enemyRigidbody.velocity.y);
-            enemySpriteRenderer.flipX = false;
-            if (transform.position.x < _leftPoint.position.x)
+            else
+            { 
+                enemyRigidbody.velocity = new Vector2(-_moveSpeed, enemyRigidbody.velocity.y);
+                enemySpriteRenderer.flipX = false;
+                if (transform.position.x < _leftPoint.position.x)
+                {
+                    movingRight = true;
+                }
+            }
+
+            if (moveCount <= 0)
             {
-                movingRight = true;
+                waitCount = Random.Range(_waitTime * .75f, _waitTime * 1.25f );
             }
+            anim.SetBool("isMoving", true);
+        } else if (waitCount > 0)
+        {
+            waitCount -= Time.deltaTime;
+            enemyRigidbody.velocity = new Vector2(0f, enemyRigidbody.velocity.y);
+            if (waitCount <= 0)
+            {
+                moveCount = Random.Range(_moveTime * .75f, _moveTime * .85f);
+            }
+            anim.SetBool("isMoving", false);
         }
     }
 }

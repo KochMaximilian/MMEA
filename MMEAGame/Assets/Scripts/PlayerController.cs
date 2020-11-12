87cf288f7624cacfee56ad7c91d52e8f86c31a07
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-11-12 21:15:58 +0200

    added moving platforms

--- MMEAGame/Assets/Scripts/PlayerController.cs
+++ MMEAGame/Assets/Scripts/PlayerController.cs
@@ -175,5 +175,21 @@ public class PlayerController : MonoBehaviour
         
         AudioManager.instance.PlaySFX(10);
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform.parent = null;
+        }
+    }
 }
 

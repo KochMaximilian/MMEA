e1410aac036d15d5cdcb2c85ab5500df9bcce422
Maximilian Koch <1710653582@stud.fh-kufstein.ac.at>
2020-11-16 21:15:59 +0200

    states

--- MMEAGame/Assets/Scripts/BossBeeController.cs
+++ MMEAGame/Assets/Scripts/BossBeeController.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossBeeController : MonoBehaviour
 {
+
+    public enum bossStates
+    {
+        shooting,
+        hurt,
+        moving
+    };
+    public bossStates currentState;
+    
     public Transform theBoss;
     public Animator anim;
     
@@ -11,11 +21,13 @@ public class BossBeeController : MonoBehaviour
     public float moveSpeed;
     public Transform leftPoint, rightPoint;
     private bool moveRight;
+    
     [Header("Shooting")]
     public GameObject bullet;
     public Transform firePoint;
     public float timeBetweenShots;
     private float shotCounter;
+    
     [Header("Boss Hurt")]
     public float hurtTime;
     private float hurtCounter;
@@ -23,12 +35,20 @@ public class BossBeeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = bossStates.shooting;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case bossStates.shooting:
+                break;
+            case bossStates.hurt:
+                break;
+            case bossStates.moving:
+                break;
+        }
     }
 }
